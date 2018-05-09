b4229fdf5a9ec43846c59db4117ed0d8e5a9f996
Rashed Abdel-Tawab <rashed@linux.com>
2018-05-09 13:16:27 -0700

    onclite: Build vendor version of libstdc++
    Change-Id: Ie43c0a6b594ecfcff393efb8a3937f04af0ee0a4

--- device.mk
+++ device.mk
@@ -127,6 +127,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     libgui_vendor \
+    libstdc++.vendor \
     GCam
 
  # Charger Images
