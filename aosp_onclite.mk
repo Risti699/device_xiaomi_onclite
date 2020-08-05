8feae2ce9aeb1e5db2ab0820ab7eb31f4efaae4d
hadaddarajat <hadaddarajat@yahoo.com>
2020-08-05 17:53:55 +0700

    onclite: Update coral patch august release
    Co-authored-by: TheSync <repo-sync@outlook.com>
    Co-authored-by: Zinadin Zidan <zidan44@pixelexperience.org>

--- aosp_onclite.mk
+++ aosp_onclite.mk
@@ -26,8 +26,8 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="onc" \
     TARGET_DEVICE="onc" \
-    PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200705.002 6506677 release-keys"
+    PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200805.001 6578210 release-keys"
 
-BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
