d2589c6114cecd10dee133f39a55319ef20b80cd
MumetNgoding <bryanhafidz@hack.id>
2020-11-27 09:19:28 +0000

    Onclite: Bringup to RevengeOs

--- revengeos_onclite.mk
+++ revengeos_onclite.mk
@@ -12,8 +12,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from onclite device
 $(call inherit-product, device/xiaomi/onclite/device.mk)
 
-# Inherit some common Pixys stuff.
-$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
+# Inherit some common RevengeOS stuff.
+$(call inherit-product, vendor/revengeos/config/common.mk)
 
 #Boot animation
 TARGET_BOOT_ANIMATION_RES := 720
@@ -22,7 +22,7 @@ TARGET_BOOT_ANIMATION_RES := 720
 TARGET_GAPPS_ARCH := arm64
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := pixys_onclite
+PRODUCT_NAME := revengeos_onclite
 PRODUCT_DEVICE := onclite
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
